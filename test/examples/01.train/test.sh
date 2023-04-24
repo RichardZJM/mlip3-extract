b0de4e9ca637ab18d647633620df959d3c9660bb
ivan <i.novikov@skoltech.ru>
2023-04-24 19:06:08 +0300

    minor in mlp_commands

--- test/examples/01.train/test.sh
+++ test/examples/01.train/test.sh
@@ -6,4 +6,4 @@ TMP_DIR=./out
 mkdir -p $TMP_DIR
 
 # Body:
-$MLP_EXE train 06.mtp train.cfg --save_to=$TMP_DIR/pot.almtp --iteration_limit=100 --al_mode=nbh
+$MLP_EXE train 06.almtp train.cfg --save_to=$TMP_DIR/pot.almtp --iteration_limit=100 --al_mode=nbh
